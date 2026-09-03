869565603769d56f3b49c9f9e08cf7dc5ad7525e
aleojeda8 <aleojeda861@gmail.com>
2026-09-03 15:02:43 -0300

    Clase 17: Validaciones y estados HTTP

--- backend/controllers/alumnos.controller.js
+++ backend/controllers/alumnos.controller.js
@@ -7,18 +7,31 @@ function obtenerAlumnos(req, res){
 function obtenerAlumno(req, res){
     const id = Number(req.params.id);
     const alumno = alumnos.find(a => a.id === id)
+    if(!alumno){
+        return res.status(404).json({Error: "Alumno no encontrado"});
+    }
     res.json(alumno);
 }
 
 function registrarAlumno(req, res){
     const alumno = req.body;
+    const {id, nombre, carrera} = req.body;
+    if( !id || !nombre || !carrera){
+        return res.status(400).json({Error: "Todos los campos son obligatorios"});
+    }
+    if(typeof nombre !== "string"){
+        return res.status(400).json({Error: "El nombre debe ser un string"});
+    }
     alumnos.push(alumno);
-    res.json({mensaje: "Alumno registrado correctamente"});
+    res.status(201).json({mensaje: "Alumno registrado correctamente"});
 }
 
 function actualizarAlumno (req, res){
     const id = Number(req.params.id);
     const alumno = alumnos.find(alumno => alumno.id === id);
+    if(!alumno){
+        return res.status(404).json({Error: "Alumno no encontrado"});
+    }
     // alumno.id = req.body.id;
     alumno.nombre = req.body.nombre;
     alumno.carrera = req.body.carrera;
@@ -27,11 +40,14 @@ function actualizarAlumno (req, res){
 
 function eliminarAlumno (req, res){
     const id = Number(req.params.id);
+    const alumno = alumnos.find(alumno => alumno.id === id);
+    if(!alumno){
+        return res.status(404).json({Error: "Alumno no encontrado"});
+    }
     const alumnosAux = alumnos.filter(alumno => alumno.id !== id);
     alumnos.length = 0;
     alumnos.push(...alumnosAux);
     res.json({mensaje: "Alumno eliminado correctamente"});
-
 }
 
 module.exports = {obtenerAlumnos, obtenerAlumno, registrarAlumno, actualizarAlumno, eliminarAlumno};
